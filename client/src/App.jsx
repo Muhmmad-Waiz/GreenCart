d7af00ee6b48003c9e8697d918ec56da86f08a87
Muhammad-Waiz <waiz35202@gmail.com>
2025-08-10 06:33:34 +0500

    HOME ERROR

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './componenets/Navbar'
 import { Route , Routes, useLocation } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import {Toaster} from 'react-hot-toast'
 import Fotter from "./componenets/Fotter";
 import {useAppContext} from "./context/AppContext";
